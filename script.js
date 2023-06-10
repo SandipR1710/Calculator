fd58986e705ea1ccb6af3c2a72ba64a9edd0de8e
Sandip Roy <sandyishero72@gmail.com>
2023-06-10 14:33:37 +0530

    functionality added except dot

--- script.js
+++ script.js
@@ -1,48 +1,105 @@
-// let expression='';
-// let dotAppeared=0;
-// let last;
-// function isOperator(tc){
-//     if(tc==='+' || tc==='-' || tc==='÷' || tc==='×' || tc==='%') return true;
-//     return false;
-// }
-// const btns=document.querySelectorAll('.btn');
-// btns.forEach(function(btn){
-//     btn.addEventListener('click',function(e){
-//         let tc=e.target.textContent;
-//         if(tc==='AC'){
-//             const exp=document.querySelector('.exp');
-//             const result=document.querySelector('.result');
-//             exp.textContent='';
-//             result.textContent='';
-//         }
-//         else if(tc==='C'){
-//             const exp=document.querySelector('.exp');
-//             let string=exp.textContent;
-//             string=string.substring(0,string.length-1);
-//             exp.textContent=string;
-//         }
-//         else if(tc==='='){
-//             const exp=document.querySelector('.exp');
-//             const result=document.querySelector('.result');
-//             exp.textContent=result.textContent;
-//             result.textContent='';
-//         }
-//         else if(isOperator(tc)){
-//             const exp=document.querySelector('.exp');
-//             const result=document.querySelector('.result');
-//             let x=exp.textContent;
-//             if(x.length>0 && isOperator(x.substring(x.length-1))){
-//                 let expr=exp.textContent;
-//                 expr=expr.substring(0,expr.length-1)+''+tc;
-//                 exp.textContent=expr;
-//                 result.textContent=result.textContent;
-//             }
-//             else if(result.text.length===0){
-//                 exp.textContent='';
-//             }
-//             else{
+let lastClicked=-1;
+const numbers=document.querySelectorAll('.num');
+numbers.forEach(function(number){
+    number.addEventListener('click',function(e){
+        const exp=document.querySelector('.exp');
+        const result=document.querySelector('.result');
+        if(lastClicked===-1 || lastClicked===0 || lastClicked===1 || lastClicked===2){
+            if(lastClicked===-1){
+                result.textContent='';
+            }
+            if(lastClicked===2){
+                exp.textContent='';
+            }
+            result.textContent=result.textContent+e.target.textContent;
+        }
+        else{
+            result.textContent=e.target.textContent;
+        }
+        lastClicked=0;
+    });
+});
+const del=document.querySelector('#erase');
+del.addEventListener('click',function(e){
+    const result=document.querySelector('.result');
+    let string=result.textContent;
+    string=string.substring(0,string.length-1);
+    result.textContent=(string.length==0 ? 0:string);
+});
+const clr=document.querySelector('.ac');
+clr.addEventListener('click',function(e){
+    const exp=document.querySelector('.exp');
+    const result=document.querySelector('.result');
+    exp.textContent='';
+    result.textContent='0';
+    lastClicked=-1;
+});
+const ops=document.querySelectorAll('.op');
+ops.forEach(function(op){
+    op.addEventListener('click',function(e){
+        const exp=document.querySelector('.exp');
+        const result=document.querySelector('.result');
+        if(lastClicked===2){
+            exp.textContent='';
+        }
+        
+        if(exp.textContent.length===0){
+            exp.textContent=result.textContent+e.target.textContent;
+        }
+        else if(lastClicked!==3){
+            let ex=exp.textContent;
+            let n=ex.length;
+            let x=parseFloat(ex.substring(0,n-1));
+            let y=parseFloat(result.textContent);
+            if(ex[n-1]=='+'){
+                exp.textContent=(Math.round((x+y) * 1000) / 1000)+e.target.textContent;
+                result.textContent=Math.round((x+y) * 1000) / 1000;
+            }
+            else if(ex[n-1]=='-'){
+                exp.textContent=(Math.round((x-y) * 1000) / 1000)+e.target.textContent;
+                result.textContent=Math.round((x-y) * 1000) / 1000;
+            }
+            else if(ex[n-1]=='÷'){
+                exp.textContent=(Math.round((x/y) * 1000) / 1000)+e.target.textContent;
+                result.textContent=Math.round((x/y) * 1000) / 1000;
+            }
+            else if(ex[n-1]=='×'){
+                exp.textContent=(Math.round((x*y) * 1000) / 1000)+e.target.textContent;
+                result.textContent=Math.round((x*y) * 1000) / 1000;
+            }
+        }
+        lastClicked=3;
+    });
+});
+const eq=document.querySelector('#equal');
+eq.addEventListener('click',function(e){
+    const exp=document.querySelector('.exp');
+    const result=document.querySelector('.result');
+    if(exp.textContent.length>0 && result.textContent.length>0 && lastClicked===0){
+        let ex=exp.textContent;
+        let n=ex.length;
+        let x=parseFloat(ex.substring(0,n-1));
+        let y=parseFloat(result.textContent);
+        if(ex[n-1]=='+'){
+            exp.textContent=exp.textContent+result.textContent;
+            result.textContent=Math.round((x+y) * 1000) / 1000;
+        }
+        else if(ex[n-1]=='-'){
+            exp.textContent=exp.textContent+result.textContent;
+            result.textContent=Math.round((x-y) * 1000) / 1000;
+        }
+        else if(ex[n-1]=='÷'){
+            exp.textContent=exp.textContent+result.textContent;
+            result.textContent=Math.round((x/y) * 1000) / 1000;
+        }
+        else if(ex[n-1]=='×'){
+            exp.textContent=exp.textContent+result.textContent;
+            result.textContent=Math.round((x*y) * 1000) / 1000;
+        }
+    }
+    lastClicked=2;
+});
+const dot=document.querySelector('.dot');
+dot.addEventListener('click',function(e){
 
-//             }
-//         }
-//     });
-// });
+});
