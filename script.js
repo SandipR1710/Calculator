ed253d2ee9811e10bbcf4f17beaa42495e392403
Sandip Roy <sandyishero72@gmail.com>
2023-06-11 11:18:43 +0530

    dot functionality used

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let lastClicked=-1;
+let dotClicked=0;
 const numbers=document.querySelectorAll('.num');
 numbers.forEach(function(number){
     number.addEventListener('click',function(e){
         const exp=document.querySelector('.exp');
         const result=document.querySelector('.result');
-        if(lastClicked===-1 || lastClicked===0 || lastClicked===1 || lastClicked===2){
-            if(lastClicked===-1){
+        if(lastClicked===-1 || lastClicked===0 || lastClicked===1 || lastClicked===2 || dotClicked===1){
+            if(lastClicked===-1 && dotClicked===0){
                 result.textContent='';
             }
             if(lastClicked===2){
@@ -23,6 +24,9 @@ const del=document.querySelector('#erase');
 del.addEventListener('click',function(e){
     const result=document.querySelector('.result');
     let string=result.textContent;
+    if(string[string.length-1]=='.'){
+        dotClicked=0;
+    }
     string=string.substring(0,string.length-1);
     result.textContent=(string.length==0 ? 0:string);
 });
@@ -33,6 +37,7 @@ clr.addEventListener('click',function(e){
     exp.textContent='';
     result.textContent='0';
     lastClicked=-1;
+    dotClicked=0;
 });
 const ops=document.querySelectorAll('.op');
 ops.forEach(function(op){
@@ -42,7 +47,11 @@ ops.forEach(function(op){
         if(lastClicked===2){
             exp.textContent='';
         }
-        
+        if(lastClicked===3){
+            let tc=exp.textContent;
+            let n=tc.length;
+            exp.textContent=tc.substring(0,n-1)+e.target.textContent;
+        }
         if(exp.textContent.length===0){
             exp.textContent=result.textContent+e.target.textContent;
         }
@@ -69,6 +78,7 @@ ops.forEach(function(op){
             }
         }
         lastClicked=3;
+        dotClicked=0;
     });
 });
 const eq=document.querySelector('#equal');
@@ -96,10 +106,27 @@ eq.addEventListener('click',function(e){
             exp.textContent=exp.textContent+result.textContent;
             result.textContent=Math.round((x*y) * 1000) / 1000;
         }
+        for(let i=0;i<result.textContent.length;i++){
+            if(result.textContent[i]=='.'){
+                dotClicked=1;
+                break;
+            }
+        }
     }
     lastClicked=2;
 });
 const dot=document.querySelector('.dot');
 dot.addEventListener('click',function(e){
-
+    const result=document.querySelector('.result');
+    const exp=document.querySelector('.exp');
+    if(dotClicked===0){
+        if(lastClicked===2){
+            exp.textContent='';
+        }
+        result.textContent=result.textContent+e.target.textContent;
+    }
+    if(lastClicked===3){
+        result.textContent='.';
+    }
+    dotClicked=1;
 });
